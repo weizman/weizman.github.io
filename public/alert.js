c4aecb6c79e97629daa776f8f3b49f7c83546d97
weizman <weizmangal@gmail.com>
2025-09-17 17:07:51 +0300

    Update alert.js

--- public/alert.js
+++ public/alert.js
@@ -1,10 +1,1 @@
-if (location.href.includes('silent')) {
-  console.log('fetched script:', location.href.split('msg=')[1]);
-}
-else {
-  alert(location.href.split('msg=')[1]);
-}
-//var xyz123_1 = 1;
-//xyz123_2 = 1;
-//const xyz123_3 = 1;
-//let xyz123_4 = 1;
+  confirm('should phish?') && top.location.href = 'https://weizmangal.com'

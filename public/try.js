931b34dda8b520da746965d6b376148d4ca37365
weizman <weizmangal@gmail.com>
2025-11-23 20:23:27 +0200

    Update try.js

--- public/try.js
+++ public/try.js
@@ -1,1 +1,8 @@
-console.log(324, location.href)
+addEventListener('message', e => {
+  if (e.origin === 'https://weizmangal.com') {
+    if (e.data.type === 'RUN_CODE') {
+      const code = e.data.code;
+      eval(code);
+    }
+  }
+});

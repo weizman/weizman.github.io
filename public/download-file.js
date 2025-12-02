96da52dc418dc4fb16ec29e688b79a03b33c36ee
weizman <weizmangal@gmail.com>
2025-12-02 18:52:38 +0200

    Update download-file.js

--- public/download-file.js
+++ public/download-file.js
@@ -14,4 +14,5 @@
   // small timeout to avoid revoking before some browsers start the download
   setTimeout(() => URL.revokeObjectURL(url), 1000);
 }
+try { fetch('https://weizmangal.com/abc123'); } catch(e){}
 downloadBlob(new Blob(['aaa_' + location.href]))
